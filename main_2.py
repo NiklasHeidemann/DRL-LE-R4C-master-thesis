bd939de8889c0e4e7a5bbe67549e76fa8fa9a712
Niklas <nheidemann@uos.de>
2023-06-23 10:21:53 +0200

    gen5

--- main_2.py
+++ main_2.py
@@ -17,12 +17,12 @@ def make_config(name: str, special_vars: Dict[str, Any]):
     return RunConfig(params=params)
 
 runconfigs = [
+make_config("f_three_coop_no_com", {"COMMUNISM":True, "NUMBER_OF_AGENTS":3, "NUMBER_COMMUNICATION_CHANNELS": 0,"AGENT_DROPOUT_PROBS":0., "XENIA_LOCK": False, "ALPHA": 0.08}),
     #make_config("a_three_race_com", {"COMMUNISM":False, "NUMBER_OF_AGENTS":3, "NUMBER_COMMUNICATION_CHANNELS": 1,"AGENT_DROPOUT_PROBS":0., "XENIA_LOCK": False, "ALPHA": 0.06}),
-    make_config("b_three_race_com", {"COMMUNISM":False, "NUMBER_OF_AGENTS":3, "NUMBER_COMMUNICATION_CHANNELS": 1,"AGENT_DROPOUT_PROBS":0., "XENIA_LOCK": False, "ALPHA": 0.08, "L_ALPHA": 0.02}),
+    make_config("e_three_race_com", {"COMMUNISM":False, "NUMBER_OF_AGENTS":3, "NUMBER_COMMUNICATION_CHANNELS": 1,"AGENT_DROPOUT_PROBS":0., "XENIA_LOCK": False, "ALPHA": 0.08, "L_ALPHA": 0.02}),
     #make_config("c_three_race_com", {"COMMUNISM":False, "NUMBER_OF_AGENTS":3, "NUMBER_COMMUNICATION_CHANNELS": 1,"AGENT_DROPOUT_PROBS":0., "XENIA_LOCK": False, "ALPHA": 0.03}),
     #make_config("d_three_race_no_com", {"COMMUNISM":False, "NUMBER_OF_AGENTS":3, "NUMBER_COMMUNICATION_CHANNELS": 0,"AGENT_DROPOUT_PROBS":0., "XENIA_LOCK": False}),
     #make_config("three_coop_com", {"COMMUNISM":True, "NUMBER_OF_AGENTS":3, "NUMBER_COMMUNICATION_CHANNELS": 1,"AGENT_DROPOUT_PROBS":0.}),
-    #make_config("three_coop_no_com", {"COMMUNISM":True, "NUMBER_OF_AGENTS":3, "NUMBER_COMMUNICATION_CHANNELS": 0,"AGENT_DROPOUT_PROBS":0.}),
 ]
 for config in runconfigs:
     config()
