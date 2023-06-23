89d88861984a505495ebf31121f0dfa50e9ed39d
Niklas <nheidemann@uos.de>
2023-06-23 11:25:17 +0200

    gen6

--- runconfig.py
+++ runconfig.py
@@ -53,7 +53,7 @@ class RunConfig:
     ENVIRONMENT_STEPS_PER_TRAINING = 500
     TRAININGS_PER_TRAINING = 8
     EPOCHS = 200000
-    SEED = 13
+    SEED = 15
     ENV_PARALLEL = 32
     FROM_SAVE = False
     RENDER = False
