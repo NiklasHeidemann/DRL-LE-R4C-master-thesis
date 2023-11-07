86a61410b625a3736a8dc623392a530e35fc09e5
Niklas <nheidemann@uos.de>
2023-11-07 15:41:29 +0100

    reuse previous

--- hpo.py
+++ hpo.py
@@ -1,10 +1,44 @@
+import os
+from collections import defaultdict
+from pathlib import Path
+
 import numpy as np
 from bayes_opt import BayesianOptimization
 
 from experiments import medium_choice
 from runconfig import make_config
+from utils.loss_logger import LossLogger
 
 
+def register_previous(optimizer: BayesianOptimization):
+    path = Path("/home/nemo/pycharmProjects/adversarial/saves/hpo/logger/")
+    path = Path("logger/")
+    all = defaultdict(list)
+    for file in os.listdir(path):
+        if not file.startswith("hpo_exp_2"):
+            continue
+        logger = LossLogger(file.rsplit("_logger",maxsplit=1)[0])
+        logger.load(path=str(path))
+        result = logger.avg_last(identifier="test_return", n=20)
+        file_parts = file.split("_")
+        params = {}
+        params["com_alpha"] = float(file_parts[3][2:])
+        params["mov_alpha"] = float(file_parts[4][2:])
+        params["learning_rate_exp"] = float(file_parts[5][2:])
+        params["batch_size_exp"] = float(file_parts[6][2:])
+        params["layer_size"] = float(file_parts[7][2:])
+        params["gamma_inverse_exp"] = float(file_parts[8][2:])
+        all[str(params)].append(result)
+        if len(all[str(params)])==3:
+            optimizer.register(params=params, target=np.median(all[str(params)]))
+    for key in all:
+        all[key] = np.median(all[key])
+    # print so that it can by copied as python code
+    print("all = {")
+    for key in all:
+        print(f"{key}: {all[key]},")
+    print("}")
+    return optimizer
 def wrapper_exp_2(com_alpha: float, mov_alpha:float, learning_rate_exp: float, batch_size_exp: int, layer_size: int, gamma_inverse_exp: float):
     name = f"hpo_exp_2_ca{com_alpha}_ma{mov_alpha}_lr{learning_rate_exp}_bs{batch_size_exp}_ls{layer_size}_gi{gamma_inverse_exp}"
     seeds = [19,20,21]
@@ -16,8 +50,12 @@ def wrapper_exp_2(com_alpha: float, mov_alpha:float, learning_rate_exp: float, b
                                                                 }) for seed in seeds]
     avg_returns = []
     for config in configs:
-        loss_logger = config()
-        avg_returns.append(loss_logger.avg_last(identifier="test_return", n=20))
+        try:
+            loss_logger = config()
+            avg_returns.append(loss_logger.avg_last(identifier="test_return", n=20))
+        except Exception as e:
+            print("Exception, aborting run with -1000", e)
+            return -1000
         print(name, "done with", avg_returns[-1])
     return np.median(avg_returns)
 
@@ -34,7 +72,8 @@ optimizer = BayesianOptimization(
     verbose=2,
     random_state=1,
 )
+register_previous(optimizer=optimizer)
 optimizer.maximize(
-    init_points=2,
+    init_points=0,
     n_iter=8,
 )
