e235e6b8e5be2c0de24f0615eb4e729e39c4d1fa
Niklas <nheidemann@uos.de>
2023-12-09 19:49:49 +0100

    readme

--- README.md
+++ README.md
@@ -1,17 +1,36 @@
 # Overview
 
 - For access to the master thesis pdf, please contact me or the resarch group supervisor.
-- the data of the run experiments can be found in the folder experiments. All experiments are named. Each folder contains the json-files with the run configurations, the metric plots, the saved tensorflow models and the logged metrics. The experiments are:
-  - LE: 4 seeds in the choice environment with 2 agents. One seed exhibited language
-  - oR4C: 4 seeds in the choice environment with 3 agents. No seed exhibited language
-  - R4C_easy: 2 agents vs 3 cooperating agents vs 3 R4C-agents x communication vs no communication in the easy random env. No run exhibited language, environment is too easy.
-  - R4C_nocom: Nocom 3 cooperating agents in harder random environments. Established baselines
-  - R4C_medium: 3 cooperating agents vs 3 R4C-agents in the medium random env. No run exhibited language, LE is too infrequent.
-  - SAC: Attempt to compare SAC with PPO. Unoptimized SAC is too bad.
-  - HPO: Attempt to optimize PPO parameters, with the available computational resources, no improvement was found, especially no LE.
-  - SocialReward: Experiment with adding social influence as reward (see Jaques et al). Only detrimental effects found.
 
 - for how to run experiments, look at main.py and experiments.py. All configurable parameters can be found in runconfig.py 
 - for running a hyperparameter search, look and execute hpo.py
 
 - for visualization of the test episodes, change the render parameter in domain.py
+
+## Experiment data
+- the data of the experiment runs can be found in the folder 'experiments'. All experiments are named. Each folder contains the json-files with the run configurations, the metric plots, the saved tensorflow models and the logged metrics. The experiments are:
+  - LE: 4 seeds in the choice environment with 2 agents. One seed exhibited language
+  - oR4C: 4 seeds in the choice environment with 3 agents. No seed exhibited language
+  - R4C_easy: 2 agents vs 3 cooperating agents vs 3 R4C-agents x communication vs no communication in the easy random env. No run exhibited language, environment is too easy.
+  - R4C_nocom: Nocom 3 cooperating agents in harder random environments. Established baselines
+  - R4C_medium: 3 cooperating agents vs 3 R4C-agents in the medium random env, 2 seeds each. No run exhibited language, LE is too infrequent.
+  - SAC: Attempt to compare SAC with PPO. Unoptimized SAC is too bad to do this systematically.
+  - HPO: Attempt to optimize PPO parameters, with the available computational resources, no improvement was found, especially no LE.
+  - SocialReward: Experiment with adding social influence as reward (see Jaques et al). Only detrimental effects found.
+
+
+## Code structure
+- top level files are executable scripts (main, hpo) or configuration utils (runconfig, experiments, domain)
+- environment contains:
+  - generator.py: three different generators that create grid world instances
+  - env.py: The actual environment, that uses a generator for each episode.
+  - render.py: Visualization, use it by changing the render parameter in domain.py
+  - reward.py: Different reward definitions
+  - stats.py: Saving all parameters of a grid world instance
+- experiments (see above)
+- language: Files for the computation of the two metrics:
+    - predictGoal: positive signaling/ProbeClassifiers
+    - SocialRewardComputer: positive listening/Social Influence
+- plotting: Utils for visualizing the metrics, only plots.py is used by the rest of the code, the rest are separate scripts
+- Training: Split in general (often abstract) training classes and the specific parts of PPO and SAC (use of PPO is very much recommended)
+- utils: Metric logging and a helper for code execution time stopping
--- plotting/custom_plots.py
+++ plotting/custom_plots.py
@@ -1,5 +1,6 @@
 from math import ceil
 
+import numpy as np
 from plotly.graph_objs import Scatter
 
 from utils.loss_logger import LossLogger, TEST_RETURNS, SOCIAL_REWARD, PREDICTED_GOAL, RETURNS
@@ -9,6 +10,9 @@ import plotly.express as px
 def plot_experiment_4(names: list[str], baselines: list[float|None], dir:str, real_names: list[str], vars=None, y_ranges=None, expname:str="",no_log:bool=False,fileapp:str=""):
     if vars is None:
         vars = [TEST_RETURNS, PREDICTED_GOAL, SOCIAL_REWARD]
+        FACTORS = [10,100,1]
+    elif vars ==[TEST_RETURNS]:
+        FACTORS = [10]
     if y_ranges is None:
         y_ranges = [[0,1],[0,1],[-5,-1]]
     values = {}
@@ -16,6 +20,7 @@ def plot_experiment_4(names: list[str], baselines: list[float|None], dir:str, re
         logger = LossLogger(run_name=name)
         logger.load(path=f"../experiments/{dir}/logger_")
         values.update({(real_names[index], var):logger.all_smoothed()[var] for var in vars})
+
     length = len(values[(real_names[0], TEST_RETURNS)]) * 10 if TEST_RETURNS in vars else len(values[(real_names[0], RETURNS)]) * 1
     x_labels = [f"Epoch (tested every 10 epochs)", f"Epoch (tested every 100 epochs)", "Epoch"] if vars!= [RETURNS] else ["Epoch"]
     y_labels = [f"Smoothed test returns", f"Probe classifier accuracy", "Social Influence"]
@@ -35,7 +40,9 @@ def plot_experiment_4(names: list[str], baselines: list[float|None], dir:str, re
             fig.update_yaxes(tickvals=[10**-x for x in range(6,0,-1)])
         for (name, var2), vals in values.items():
             if var==var2 and len(vals)>0:
-                fig.add_trace(Scatter(x=list(range(0,length,ceil(length/len(vals)))),y=vals, name=name))
+
+
+                fig.add_trace(Scatter(x=list(range(0,length,FACTORS[index])),y=vals, name=name))
         if baselines[index] is not None:
             fig.add_trace(Scatter(y=[baselines[index]]*length,x=list(range(0,length,1)),name="baseline", line=dict(color="black", dash="dash")))
         fig.update_xaxes(title_text=x_labels[index])
@@ -47,14 +54,18 @@ def plot_experiment_4(names: list[str], baselines: list[float|None], dir:str, re
 
 if __name__ == "__main__":
     seed_names = ["seed=30", "seed=31", "seed=32", "seed=33"]
-    '''plot_experiment_4(expname="2 agents",names=["exp_4_a_com_easy_thomp", "exp_4_b_com_easy_thomp", "exp_4_c_com_easy_thomp", "exp_4_d_com_easy_thomp"],baselines=[0.25,0.25,None],dir="exp4",real_names=seed_names)
-    plot_experiment_4(expname="oR4C",names=["exp_5_a_com_easy_thomp", "exp_5_b_com_easy_thomp", "exp_5_c_com_easy_thomp", "exp_5_d_com_easy_thomp"],baselines=[1/3,0.2,None],dir="exp5",real_names=seed_names)
-    plot_experiment_4(expname="env=easy",names=["exp_6_a_com_r_easy_thomp", "exp_6_b_no_com_r_easy_thomp", "exp_6_c_com_r_3c_easy_thomp", "exp_6_d_no_com_r_3c_easy_thomp", "exp_6_e_com_r_3v_easy_thomp","exp_6_f_no_com_r_3v_easy_thomp"],baselines=[None,0.25,None],dir="exp6",
+    plot_experiment_4(expname="2 agents",names=["exp_4_a_com_easy_thomp", "exp_4_b_com_easy_thomp", "exp_4_c_com_easy_thomp", "exp_4_d_com_easy_thomp"],baselines=[0.25,0.25,None],dir="LE_chapter5.3",real_names=seed_names)
+    plot_experiment_4(expname="oR4C",names=["exp_5_a_com_easy_thomp", "exp_5_b_com_easy_thomp", "exp_5_c_com_easy_thomp", "exp_5_d_com_easy_thomp"],baselines=[1/3,0.2,None],dir="oR4C_chapter5.3",real_names=seed_names)
+    plot_experiment_4(expname="env=easy",names=["exp_6_a_com_r_easy_thomp", "exp_6_b_no_com_r_easy_thomp", "exp_6_c_com_r_3c_easy_thomp", "exp_6_d_no_com_r_3c_easy_thomp", "exp_6_e_com_r_3v_easy_thomp","exp_6_f_no_com_r_3v_easy_thomp"],baselines=[None,0.25,None],dir="R4C_easy_chapter5.4",
                       real_names=["Com., 2 ag.","No com., 2 ag.","Com., 3 ag.","No com., 3 ag.","Com., 3 ag.,<br>R4C","No com., 3 ag.,<br>R4C"]),
-    plot_experiment_4(expname="env=medium",names=["exp_8_a_com_3c_middle", "exp_8_b_com_3c_middle", "exp_8_c_com_3v_middle","exp_8_d_com_3v_middle" ],y_ranges=[[-1.5,1],[0,1],[-5,-1]],baselines=[None,0.05,None],dir="exp8",real_names=["coop i)", "coop ii)", "R4C i)", "R4C ii)"]),
-    plot_experiment_4(expname="no-com",names=["exp_7_a_no_com_r_middle", "exp_7_b_no_com_r_harder", "exp_7_c_no_com_r_hard"],y_ranges=[[-1.5,1],[0,1],[-5,-1]],baselines=[None,None,None],dir="exp7",
+    plot_experiment_4(expname="env=medium",names=["exp_8_a_com_3c_middle", "exp_8_b_com_3c_middle", "exp_8_c_com_3v_middle","exp_8_d_com_3v_middle" ],y_ranges=[[-1.5,1],[0,1],[-5,-1]],baselines=[None,0.05,None],dir="R4C_medium_chapter5.4",real_names=["coop i)", "coop ii)", "R4C i)", "R4C ii)"]),
+    plot_experiment_4(expname="no-com",names=["exp_7_a_no_com_r_middle", "exp_7_b_no_com_r_harder", "exp_7_c_no_com_r_hard"],y_ranges=[[-1.5,1],[0,1],[-5,-1]],baselines=[None,None,None],dir="R4C_nocom_chapter5.4",
                       real_names=["medium", "hard", "very hard"])
-    plot_experiment_4(expname="Social Reward Weights",names=["exp_9_a_com_easy_thomp", "exp_9_b_com_easy_thomp", "exp_9_c_com_easy_thomp","exp_9_d_com_easy_thomp","exp_9_e_com_easy_thomp","exp_9_f_com_easy_thomp" ],y_ranges=[[-1.5,1],[0,1],[-5,-1]],baselines=[0.25,0.25,None],dir="exp9",real_names=["w=0.01","w=0.05","w=0.1","w=1","w=10","w=50"]),
-    '''
-    plot_experiment_4(expname="Test returns / env=choice (com)",names=["sac_choice"],y_ranges=[[0,1]],baselines=[0.25],dir="sac",real_names=["SAC com."], vars=[TEST_RETURNS],fileapp="_1")
-    plot_experiment_4(expname="Test returns / env=random (no. com)",names=["sac_exp_6_b_no_com_r_easy_thomp"],y_ranges=[[-1.5,1.2]],baselines=[2/3],dir="sac",real_names=["SAC no com."], vars=[TEST_RETURNS],fileapp="_2")
+
+    plot_experiment_4(expname="Social Reward Weights",
+                      names=["exp_9_a_com_easy_thomp", "exp_9_b_com_easy_thomp", "exp_9_c_com_easy_thomp",
+                             "exp_9_d_com_easy_thomp", "exp_9_e_com_easy_thomp", "exp_9_f_com_easy_thomp"],
+                      y_ranges=[[-1.5, 1], [0, 1], [-5, -1]], baselines=[0.25, 0.25, None], dir="SocialReward_chapter5.1",
+                      real_names=["w=0.01", "w=0.05", "w=0.1", "w=1", "w=10", "w=50"]),
+    plot_experiment_4(expname="Test returns / env=choice (com)",names=["sac_choice"],y_ranges=[[-0.2,1]],baselines=[0.25],dir="sac_chapter5.1",real_names=["SAC com."], vars=[TEST_RETURNS],fileapp="_1")
+    plot_experiment_4(expname="Test returns / env=random (no. com)",names=["sac_exp_6_b_no_com_r_easy_thomp"],y_ranges=[[-1.5,1.2]],baselines=[2/3],dir="sac_chapter5.1",real_names=["SAC no com."], vars=[TEST_RETURNS],fileapp="_2")
